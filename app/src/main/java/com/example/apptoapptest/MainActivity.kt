0922f72dbd8ff9bd31a13398880988aaf723edcd
Nguyen Thai Lam <v.lamnt34@vinid.net>
2019-10-24 22:41:35 +0700

    Add signature field to example app

--- app/src/main/java/com/example/apptoapptest/MainActivity.kt
+++ app/src/main/java/com/example/apptoapptest/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.testsdk.VinIDPayParams
 import kotlinx.android.synthetic.main.activity_main.*
 import com.example.testsdk.VinIDPaySdk
@@ -16,11 +17,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //
         mTxtView.setOnClickListener {
-            val params = VinIDPayParams.Builder()
-                .setOrderId(mEditTxt.text.toString())
-                .setSignature("ac")
-                .build()
-            VinIDPaySdk.getInstance(params).startPaymentForResult(this)
+            if (mEditTxt.text.isEmpty()) {
+                Toast.makeText(this, "Ko duoc bo trong order id", Toast.LENGTH_SHORT).show()
+            } else if (mEditTxt2.text.isEmpty()) {
+                Toast.makeText(this, "Ko duoc bo trong signature", Toast.LENGTH_SHORT).show()
+            } else {
+                val params = VinIDPayParams.Builder()
+                    .setOrderId(mEditTxt.text.toString())
+                    .setSignature(mEditTxt2.text.toString())
+                    .build()
+                VinIDPaySdk.getInstance(params).startPaymentForResult(this)
+            }
         }
     }
 
@@ -32,7 +39,7 @@ class MainActivity : AppCompatActivity() {
                     println("ZZLL key $it | data ${data.extras?.get(it)}")
                 }
                 val status = data?.getStringExtra(VinIDPayConst.EXTRA_STRING_ORDER_STATUS)
-                mTxtView.text = status
+                returnedValue.text = status
             }
         }
     }
